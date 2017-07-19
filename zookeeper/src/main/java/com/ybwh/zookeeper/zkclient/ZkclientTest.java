c2c7c41bc18830d95bc07739b2189e65545835b4
youbeiwuhuan <fan794919764@126.com>
2017-07-19 21:42:14 +0800

    watcher补充

--- zookeeper/src/main/java/com/ybwh/zookeeper/zkclient/ZkclientTest.java
+++ zookeeper/src/main/java/com/ybwh/zookeeper/zkclient/ZkclientTest.java
@@ -12,6 +12,7 @@ import org.I0Itec.zkclient.exception.ZkInterruptedException;
 import org.apache.zookeeper.data.ACL;
 import org.apache.zookeeper.data.Stat;
 import org.junit.Test;
+import org.w3c.dom.ls.LSException;
 
 public class ZkclientTest {
 
@@ -78,7 +79,7 @@ public class ZkclientTest {
 			for (String s : children) {
 				System.out.println(s);
 			}
-			
+
 		} catch (ZkException e) {
 			e.printStackTrace();
 		} finally {
@@ -86,115 +87,234 @@ public class ZkclientTest {
 		}
 
 	}
-	
+
 	@Test
-	public void testReadData(){
+	public void testReadData() {
 		ZkClient zkclient = new ZkClient("localhost:2181", 5000);
 
 		try {
-			
-			String data =zkclient.readData("/test", true);//数据类型必须对应，不然抛异常
+
+			String data = zkclient.readData("/test", true);// 数据类型必须对应，不然抛异常
 			System.out.println(data);
-			
+
 		} catch (ZkException e) {
 			e.printStackTrace();
 		} finally {
 			zkclient.close();
 		}
 	}
-	
+
 	@Test
-	public void testWriteData(){
+	public void testWriteData() {
 		ZkClient zkclient = new ZkClient("localhost:2181", 5000);
 
 		try {
-			zkclient.writeData("/test", "dfsfs333333333333");
+			zkclient.writeData("/kkk", "dfsfs333333333333");
 			System.out.println("write success");
-			
+
 		} catch (ZkException e) {
 			e.printStackTrace();
 		} finally {
 			zkclient.close();
 		}
 	}
-	
+
 	@Test
-	public void testNodeWatcher(){
-		
+	public void testNodeWatcher() {
+
 		ZkClient zkclient = new ZkClient("localhost:2181", 5000);
 
+		IZkDataListener dataListener = new IZkDataListener() {
+
+			@Override
+			public void handleDataChange(String dataPath, Object data) throws Exception {
+				System.out.println("handleDataChange " + dataPath + " " + data);
+			}
+
+			@Override
+			public void handleDataDeleted(String dataPath) throws Exception {
+				System.out.println("handleDataDeleted " + dataPath);
+			}
+
+		};
+
 		try {
-			zkclient.subscribeDataChanges("/kkk", new IZkDataListener(){
+			zkclient.subscribeDataChanges("/kkk", dataListener);
+			zkclient.subscribeDataChanges("/kkk", dataListener);// 同一节点重复注册同一个对象不会触发多次
+			zkclient.subscribeDataChanges("/usr", dataListener);
 
-				@Override
-				public void handleDataChange(String dataPath, Object data) throws Exception {
-					System.out.println("handleDataChange "+dataPath+" "+data);
-				}
-
-				@Override
-				public void handleDataDeleted(String dataPath) throws Exception {
-					System.out.println("handleDataDeleted "+dataPath);
-				}
-				
-			});
-			
-			
 			try {
 				Thread.sleep(2000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			
+
 			zkclient.writeData("/kkk", "33333");
-//			zkclient.delete("/kkk");
-			
-			
-			
-			
+			zkclient.writeData("/usr", "33333");
+			// zkclient.delete("/kkk");
+
 			try {
 				Thread.sleep(200000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			
+
 		} catch (ZkException e) {
 			e.printStackTrace();
 		} finally {
 			zkclient.close();
 		}
-		
+
 	}
-	
-	
+
 	@Test
-	public void testChildrenWatcher(){
-		
+	public void testCancleNodeWatcher() {
+
+		ZkClient zkclient = new ZkClient("localhost:2181", 5000);
+
+		try {
+
+			IZkDataListener dataListener = new IZkDataListener() {
+
+				@Override
+				public void handleDataChange(String dataPath, Object data) throws Exception {
+					System.out.println("handleDataChange " + dataPath + " " + data);
+				}
+
+				@Override
+				public void handleDataDeleted(String dataPath) throws Exception {
+					System.out.println("handleDataDeleted " + dataPath);
+				}
+
+			};
+
+			// zkclient.subscribeDataChanges("/kkk", dataListener);
+			zkclient.subscribeDataChanges("/usr", dataListener);
+
+			zkclient.writeData("/kkk", "33333");
+			zkclient.writeData("/usr", "33333");
+
+			zkclient.unsubscribeDataChanges("/kkk", dataListener);
+
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			zkclient.writeData("/kkk", "33333");
+			zkclient.writeData("/usr", "33333");
+			// zkclient.delete("/kkk");
+
+			try {
+				Thread.sleep(200000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		} catch (ZkException e) {
+			e.printStackTrace();
+		} finally {
+			zkclient.close();
+		}
+
+	}
+
+	@Test
+	public void testChildrenChange() {
+
+		ZkClient zkclient = new ZkClient("localhost:2181", 5000);
+
+		IZkDataListener dataListener = new IZkDataListener() {
+
+			@Override
+			public void handleDataChange(String dataPath, Object data) throws Exception {
+				System.out.println("handleDataChange " + dataPath + " " + data);
+			}
+
+			@Override
+			public void handleDataDeleted(String dataPath) throws Exception {
+				System.out.println("handleDataDeleted " + dataPath);
+			}
+
+		};
+
+		IZkChildListener childListener = new IZkChildListener() {
+
+			@Override
+			public void handleChildChange(String parentPath, List<String> currentChilds) throws Exception {
+				System.out.println("ChildChanges" + parentPath + " " + currentChilds);
+
+				for (String c : currentChilds) {
+					// System.out.println(parentPath+"/"+c);
+					zkclient.subscribeDataChanges(parentPath + "/" + c, dataListener);
+				}
+
+			}
+		};
+
+		try {
+
+			List<String> cPath = zkclient.getChildren("/kkk");
+			for (String p : cPath) {
+				zkclient.subscribeDataChanges("/kkk/" + p, dataListener);
+
+			}
+
+			zkclient.subscribeChildChanges("/kkk", childListener);
+
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			zkclient.writeData("/kkk/ddd", "33333");
+			// zkclient.delete("/kkk");
+
+			try {
+				Thread.sleep(200000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		} catch (ZkException e) {
+			e.printStackTrace();
+		} finally {
+			zkclient.close();
+		}
+
+	}
+
+	@Test
+	public void testChildrenWatcher() {
+
 		ZkClient zkclient = new ZkClient("localhost:2181", 5000);
 
 		try {
 			zkclient.subscribeChildChanges("/kkk", new IZkChildListener() {
-				//创建和删除子节点会触发这里
+				// 创建和删除子节点会触发这里
 				@Override
 				public void handleChildChange(String parentPath, List<String> currentChilds) throws Exception {
-					System.out.println("ChildChanges"+parentPath+" "+currentChilds);
-					
+					System.out.println("ChildChanges" + parentPath + " " + currentChilds);
+
 				}
 			});
-			
+
+			// zkclient.createPersistent("/kkk/eee");
 
 			try {
 				Thread.sleep(200000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			
+
 		} catch (ZkException e) {
 			e.printStackTrace();
 		} finally {
 			zkclient.close();
 		}
-		
+
 	}
-	
 
 }
