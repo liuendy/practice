12c58f4b7a67888812eed169931e176d8d0af7ab
youbeiwuhuan <fan794919764@126.com>
2017-09-10 17:03:53 +0800

    公平排他锁

--- concurrent/README.md
+++ concurrent/README.md
@@ -1,4 +1,7 @@
-# concurrent
+#  java concurrent包原理及应用
+
+
+### CLH队列，CLH算法实现
 
 
 Java并发包的使用练习
---
+++ concurrent/src/main/java/com/ybwh/concurrent/aqs/FairExclusiveLock.java
@@ -0,0 +1,11 @@
+package com.ybwh.concurrent.aqs;
+
+/**
+ * 公平排他锁
+ * 
+ * @author fanbeibei
+ *
+ */
+public class FairExclusiveLock {
+ 
+}
