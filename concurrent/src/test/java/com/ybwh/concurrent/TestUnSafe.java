b368836e5801bff7a8f76c26733912e8dacc673d
youbeiwuhuan <fan794919764@126.com>
2017-12-14 18:04:17 +0800

    获取UnSafe类

--- concurrent/src/test/java/com/ybwh/concurrent/TestUnSafe.java
+++ concurrent/src/test/java/com/ybwh/concurrent/TestUnSafe.java
@@ -1,11 +1,32 @@
 package com.ybwh.concurrent;
 
+import java.lang.reflect.Field;
+
 import sun.misc.Unsafe;
 
 public class TestUnSafe {
 
 	public static void main(String[] args) {
-		Unsafe unsafe = Unsafe.getUnsafe();
+		/*AccessController.doPrivileged(new PrivilegedAction<Object>() {
+            public Object run() {
+            	Unsafe unsafe = Unsafe.getUnsafe();
+        		System.out.println(unsafe);
+        		return unsafe;
+            }
+        });*/
+		Unsafe unsafe = getUnsafe();
 		System.out.println(unsafe);
+		
 	}
+	
+	public static Unsafe getUnsafe() {  
+        try {  
+            Field field = Unsafe.class.getDeclaredField("theUnsafe");  
+            field.setAccessible(true);  
+            return (Unsafe)field.get(null);  
+              
+        } catch (Exception e) {  
+        }  
+        return null;  
+    }  
 }
