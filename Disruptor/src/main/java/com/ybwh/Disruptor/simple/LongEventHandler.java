7a2ab206ac82654ae8f7e9ac870d19b0824817b6
youbeiwuhuan <fan794919764@163.com>
2018-05-22 12:00:31 +0800

    Disruptor

---
+++ Disruptor/src/main/java/com/ybwh/Disruptor/simple/LongEventHandler.java
@@ -0,0 +1,10 @@
+package com.ybwh.Disruptor.simple;
+
+import com.lmax.disruptor.EventHandler;
+
+public class LongEventHandler implements EventHandler<LongEvent> { 
+    @Override 
+    public void onEvent(LongEvent event, long sequence, boolean endOfBatch) throws Exception { 
+        System.out.println(event.getValue()); 
+    } 
+} 
