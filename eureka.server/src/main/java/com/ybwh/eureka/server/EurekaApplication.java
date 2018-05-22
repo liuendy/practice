1ec37e134b7582b36ec48eb59a7302ef65d8f268
youbeiwuhuan <fan794919764@163.com>
2018-05-22 17:19:10 +0800

    eureka.client

--- eureka.server/src/main/java/com/ybwh/eureka/server/EurekaApplication.java
+++ eureka.server/src/main/java/com/ybwh/eureka/server/EurekaApplication.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication // Spring Boot 应用标识  
 @EnableEurekaServer // Eureka Server 标识并会自动化读取相关配置。  
 public class EurekaApplication {  
-    public static void main(String[] args) {  
+    public static void main(String[] args) {
         // 程序启动入口  
         // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件  
         SpringApplication.run(EurekaApplication.class, args);  
--- springboot2/src/test/java/com/ybwh/springboot2/Springboot2ApplicationTests.java
+++ springboot2/src/test/java/com/ybwh/springboot2/Springboot2ApplicationTests.java
@@ -2,16 +2,23 @@ package com.ybwh.springboot2;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ybwh.springboot2.demo.dao.AreaDao;
+
+import junit.framework.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Springboot2ApplicationTests {
-	
+	@Autowired
+	AreaDao dao;
 
 	@Test
 	public void contextLoads() {
+		Assert.assertNotNull(dao);
 	}
 
 }
