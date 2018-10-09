1dfe6967d00f6710479c469daf0c5f143c5d6bc5
youbeiwuhuan <fan794919764@163.com>
2018-10-09 19:08:38 +0800

    @DateTimeFormat

--- springboot1/src/main/java/com/ybwh/springboot1/controller/TestController.java
+++ springboot1/src/main/java/com/ybwh/springboot1/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.ybwh.springboot1.controller;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +21,16 @@ public class TestController {
 	@ResponseBody
 	public Response<Object> test(@RequestBody String data) {
 		System.out.println(data);
+
+		return new Response<Object>(Constant.FAIL_CODE, " test response");
+
+	}
+	
+	
+	@RequestMapping(value = "/testDate")
+	@ResponseBody
+	public Response<Object> testDate(@RequestParam("date") @DateTimeFormat(pattern="yyyy-MM-dd") Date date) {
+		System.out.println(date);
 
 		return new Response<Object>(Constant.FAIL_CODE, " test response");
 
