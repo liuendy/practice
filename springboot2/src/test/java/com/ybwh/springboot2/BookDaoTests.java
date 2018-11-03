1655fe5d94cbba42ba22699be62c854cfacc5c0a
youbeiwuhuan <fan794919764@163.com>
2018-11-04 02:01:24 +0800

    更新

---
+++ springboot2/src/test/java/com/ybwh/springboot2/BookDaoTests.java
@@ -0,0 +1,83 @@
+package com.ybwh.springboot2;
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ybwh.springboot2.demo.dao.BookDao;
+import com.ybwh.springboot2.demo.entity.Book;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//@TestPropertySource(properties= {"spring.config.location=E:/application.yml"})
+public class BookDaoTests {
+	@Autowired
+	BookDao dao;
+
+	@Test
+	public void contextLoads() {
+		try {
+			Assert.assertNotNull(dao);
+			
+			System.out.println(dao.selectById(1));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	@Test
+	public void testInsert() {
+		//插入返回主键
+		try {
+			Assert.assertNotNull(dao);
+			
+			Book b = new Book();
+			b.setBookName("算法导论");
+			b.setBookCode("sfdl");
+			b.setCategory(1);
+			b.setIsDel(0);
+			
+			Date now = new Date();
+			b.setCreateTime(now);
+			b.setUpdateTime(now);
+			b.setCreateUser(0);
+			b.setUpdateUser(0);
+			
+			dao.insert(b);
+			System.out.println(b.getId());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	@Test
+	public void testUpdate() {
+		//更新
+		try {
+			Assert.assertNotNull(dao);
+			
+			Book b = new Book();
+			b.setId(3);
+			b.setBookCode("002");
+			
+			Date now = new Date();
+			b.setUpdateTime(now);
+			
+			dao.updateById(b);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
