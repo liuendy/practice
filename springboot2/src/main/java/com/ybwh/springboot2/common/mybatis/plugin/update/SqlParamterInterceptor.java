cb803ea92cb40aea452504a8d6351b576e41190e
youbeiwuhuan <fan794919764@163.com>
2018-11-05 11:23:45 +0800

    解析sql

--- springboot2/src/main/java/com/ybwh/springboot2/common/mybatis/plugin/update/SqlParamterInterceptor.java
+++ springboot2/src/main/java/com/ybwh/springboot2/common/mybatis/plugin/update/SqlParamterInterceptor.java
@@ -1,15 +1,20 @@
 package com.ybwh.springboot2.common.mybatis.plugin.update;
 
+import java.io.StringReader;
 import java.lang.reflect.Field;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
+
 import org.apache.ibatis.executor.parameter.ParameterHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.ParameterMapping;
 import org.apache.ibatis.mapping.ParameterMode;
+import org.apache.ibatis.mapping.SqlCommandType;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
@@ -20,40 +25,66 @@ import org.apache.ibatis.scripting.defaults.DefaultParameterHandler;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.type.TypeHandlerRegistry;
 
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserManager;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.delete.Delete;
+import net.sf.jsqlparser.statement.insert.Insert;
+import net.sf.jsqlparser.statement.update.Update;
+
 /**
  * 获取带?占位符的sql及对应顺序参数的拦截器
  *
  */
-@Intercepts({ 
-	@Signature(type = ParameterHandler.class, 
-			method = "setParameters", 
-			args = { PreparedStatement.class }) 
-})
+@Intercepts({ @Signature(type = ParameterHandler.class, method = "setParameters", args = { PreparedStatement.class }) })
 public class SqlParamterInterceptor implements Interceptor {
+	
+	private static final Set<String> targetTables = new HashSet<>();
+	static {
+		targetTables.add("book");
+	}
+	
 
 	@Override
 	public Object intercept(Invocation invocation) throws Throwable {
-		
+
 		Object target = invocation.getTarget();
-		if (!(target instanceof DefaultParameterHandler)) {//mybatis中ParameterHandler只有一个实现类DefaultParameterHandler
-			return null;
+		if (!(target instanceof DefaultParameterHandler)) {// mybatis中ParameterHandler只有一个实现类DefaultParameterHandler
+			return invocation.proceed();
+		}
+
+		DefaultParameterHandler hander = (DefaultParameterHandler) target;
+
+		// //获取DefaultParameterHandler中的5个属性
+		MappedStatement mappedStatement = getMappedStatement(hander);
+		SqlCommandType sqlType = mappedStatement.getSqlCommandType();
+		if (!SqlCommandType.UPDATE.equals(sqlType) && !SqlCommandType.INSERT.equals(sqlType)
+				&& !SqlCommandType.DELETE.equals(sqlType)) {//只处理更新操作
+			return invocation.proceed();	
+		}
+
+		BoundSql boundSql = getBoundSql(hander);
+		String sql = boundSql.getSql();
+		
+		String tableName = getTableName(sql);
+		if(!targetTables.contains(tableName)) {//只处理目标表
+			return invocation.proceed();
 		}
 		
-		DefaultParameterHandler hander = (DefaultParameterHandler) target;
+		//按顺序获取更新列
+		List<String> column = getColmuns(sql);
 		
-		// //获取DefaultParameterHandler中的5个属性
-		Class<?> clz = hander.getClass();
-		Field f = clz.getDeclaredField("mappedStatement");
-		f.setAccessible(true);
-		MappedStatement mappedStatement = (MappedStatement) f.get(hander);
+		
+		
+
 		Configuration configuration = mappedStatement.getConfiguration();
 		TypeHandlerRegistry typeHandlerRegistry = mappedStatement.getConfiguration().getTypeHandlerRegistry();
-		f = clz.getDeclaredField("boundSql");
-		f.setAccessible(true);
-		BoundSql boundSql = (BoundSql) f.get(hander);
 		Object parameterObject = hander.getParameterObject();
-		//存储按参数顺序解析出的参数值
-		List<Object> columnValues = new ArrayList<Object>();
+
+		// 存储按参数顺序解析出的参数值
+		List<Object> columnValues = new ArrayList<>();
+
 		// 迭代得到sql参数值
 		List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
 		if (parameterMappings != null) {
@@ -77,15 +108,54 @@ public class SqlParamterInterceptor implements Interceptor {
 				}
 			}
 		}
-		
-		
-		System.out.println(boundSql.getSql());
+
+		System.out.println(sql);
 		System.out.println(columnValues);
-		
-		
+
 		// 重写ErrorContext中sql的内容,使其带上参数信息
-//		ErrorContext.instance().sql(boundSql.getSql() + " parameters:" + this.getParameterValueString(columnValues));
+		// ErrorContext.instance().sql(boundSql.getSql() + " parameters:" +
+		// this.getParameterValueString(columnValues));
 		return invocation.proceed();
+	}
+
+	private List<String> getColmuns(String sql) throws JSQLParserException {
+		CCJSqlParserManager parserManager = new CCJSqlParserManager();
+		Statement stmt = parserManager.parse(new StringReader(sql));
+		
+		if(stmt instanceof Update) {
+			Update update = (Update) stmt;
+			List<Column> columnList = update.getColumns();
+//			return columnList.stream().map(mapper)
+		}
+		
+		if(stmt instanceof Insert) {
+			Insert insert = (Insert) stmt;
+			List<Column> columnList =  insert.getColumns();
+		}
+		
+		return null;
+	}
+
+	private String getTableName(String sql) throws JSQLParserException {
+		CCJSqlParserManager parserManager = new CCJSqlParserManager();
+		Statement stmt = parserManager.parse(new StringReader(sql));
+		if(stmt instanceof Update) {
+			Update update = (Update) stmt;
+			return update.getTables().get(0).getName();
+		}
+		
+		if(stmt instanceof Insert) {
+			Insert insert = (Insert) stmt;
+			return insert.getTable().getName();
+		}
+		
+		if(stmt instanceof Delete) {
+			Delete delete = (Delete) stmt;
+			return delete.getTable().getName();
+			
+		}
+		
+		return null;
 	}
 
 	@Override
@@ -96,4 +166,23 @@ public class SqlParamterInterceptor implements Interceptor {
 	@Override
 	public void setProperties(Properties properties) {
 	}
+
+	private BoundSql getBoundSql(DefaultParameterHandler hander)
+			throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+		Class<?> clz = DefaultParameterHandler.class;
+		Field f = clz.getDeclaredField("boundSql");
+		f.setAccessible(true);
+		BoundSql boundSql = (BoundSql) f.get(hander);
+		return boundSql;
+	}
+
+	private MappedStatement getMappedStatement(DefaultParameterHandler hander)
+			throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Class<?> clz = DefaultParameterHandler.class;
+		Field f = clz.getDeclaredField("mappedStatement");
+		f.setAccessible(true);
+		MappedStatement mappedStatement = (MappedStatement) f.get(hander);
+		return mappedStatement;
+	}
+
 }
